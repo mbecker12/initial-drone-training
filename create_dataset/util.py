31e92ecee4f4f45264b225467f976fe36b55d825
Marvin Becker <marvin.becker2@inovex.de>
2020-09-20 01:13:54 +0200

    restructure

--- create_dataset/util.py
+++ create_dataset/util.py
@@ -1,14 +1,15 @@
 import numpy as np
 
-def set_random_drone_position():
-    xy = 200 * np.random.random_sample(size=(2, 1)) - 100
+def set_random_drone_position(a=-100, b=100):
+    
+    xy = (b - a) * np.random.random_sample(size=(2, 1)) - b
     z = 10 + 4 * np.random.random_sample(size=(1, 1)) - 2
     
     drone_pos = np.concatenate((xy, z), axis=0)
     return drone_pos
 
-def set_random_drone_velocity():
-    vel = 10 * np.random.random_sample(size=(3, 1)) - 5
+def set_random_drone_velocity(a=-5, b=5):
+    vel = (b - a) * np.random.random_sample(size=(3, 1)) - b
     return vel
 
 def set_random_drone_angle():
@@ -19,8 +20,8 @@ def set_random_drone_angular_velocity():
     angv = 4 * np.random.random_sample(size=(3, 1)) - 2
     return angv
 
-def set_random_coin_position():
-    xy = 200 * np.random.random_sample(size=(2, 1)) - 100
+def set_random_coin_position(a=-100, b=100):
+    xy = (b - a) * np.random.random_sample(size=(2, 1)) - b
     z = 10 + 4 * np.random.random_sample(size=(1, 1)) - 2
 
     coin_pos = np.concatenate((xy, z), axis=0)
